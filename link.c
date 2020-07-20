a7f4e6bb59e655c3db028af62599d072a36d45f6
Rashmi Bachani <echorashmi@gmail.com>
2020-07-20 13:04:16 -0400

    Extending the provided linked list to add more properties to it

--- link.c
+++ link.c
@@ -1,19 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Represents a node
 typedef struct node
 {
-    int number;
+    int roll_number;
+    int age;
     struct node *next;
 }
 node;
 
 int main(void)
 {
+    //Create a list of size 0:
+    node *list = NULL;
+    
+    //Add number to list:
     node *n = malloc(sizeof(node));
-    if (n != NULL)
+    
+    if (n == NULL)
     {
-        n->number = 2;
-        n->next = NULL;
+        return 1;
+    }
+    n->roll_number = 1;
+    n->age = 10;
+    n->next = NULL;
+    list = n;
+    
+    //Add number to list:
+    n = malloc(sizeof(node));
+    
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->roll_number = 2;
+    n->age = 30;
+    n->next = NULL;
+    list->next = n;
+    
+    //Add number to list:
+    n = malloc(sizeof(node));
+    
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->roll_number = 3;
+    n->age = 50;
+    n->next = NULL;
+    list->next->next = n;
+    
+    //Print List:
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%i: %i\n", tmp->roll_number, tmp->age);
+    }
+    
+    //Free List:
+    while(list != NULL)
+    {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
