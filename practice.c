87cebb3711e80bbc86a599e7c8fc03b54364adc4
Rashmi Bachani <echorashmi@gmail.com>
2020-07-20 01:03:22 -0400

    Reading raw file into memory - 2 ways: Full file, and 512 bytes at a time only.

--- practice.c
+++ practice.c
@@ -8,7 +8,49 @@
 int main(int argc, char *argv[])
 {
     /*
-    //How to Create multiple JPEG files using a For Loop:
+    
+    Load the entire card.raw file into memory:
+    
+    char * buffer = 0;
+    long length;
+    FILE * f = fopen ("card.raw", "rb");
+    
+    if (f)
+    {
+        fseek (f, 0, SEEK_END);
+        length = ftell (f);
+        fseek (f, 0, SEEK_SET);
+        buffer = malloc (length);
+        if (buffer)
+        {
+            fread (buffer, 1, length, f);
+        }
+        fclose (f);
+    }
+    */
+    
+    /*
+    
+    Read file 512 bytes at a time into memory:
+    
+    FILE * fp = fopen("card.raw", "rb");
+    if (fp)
+    {
+        void * buffer = malloc(512);
+        while (fread(buffer, 512, 1, fp) == 1)
+        {
+            printf("%s\n", buffer);
+        }
+        fclose(fp);
+        free(buffer);
+    }
+    
+    */
+    
+    
+    /*
+    How to Create multiple JPEG files using a For Loop:
+    
         for(int i = 0; i < 10; i++)
         {
             char filename[10];
